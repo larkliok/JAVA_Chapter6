f003a8f92824cbc228bb0c7863f88ad474371cd9
larkliok <lark_liok@naver.com>
2013-11-22 17:24:38 +0900

    2013년10월10일(JAVAChap6장)
    2013년10월10일(JAVAChap6장)

---
+++ 20131010/ChackingAccount.java
@@ -0,0 +1,25 @@
+/**
+ * 직불 계좌 클래스
+ * @author 20131010
+ *
+ */
+public class ChackingAccount extends Account{
+	String cardNO;
+	
+	int pay(String No, int amouny) {
+	
+		int balance = 0;
+		
+		return tempBalance = 0;
+		if(this.cardN0 == cardNo && tempblant);
+	}cath (Exception e) {
+		e.printStackTrace0;
+	}
+	if(this.cardNo) == cardN0 && tempBalance > amount){
+		return tempBalance;
+		
+	}
+	
+	return 0;
+       
+}
